3af62a9ad7b4935633b6a0bec84cf963a081d596
Dagmara Grabowska <dagmarag0@gmail.com>
2023-08-09 10:41:02 +0200

    add random lines on the canvas

---
+++ script.js
@@ -0,0 +1,29 @@
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+canvas.width = 800;
+canvas.height = 850;
+
+// global settings
+ctx.lineWidth = 10;
+ctx.strokeStyle = "magenta";
+
+class Line {
+    constructor(canvas) {
+        this.canvas = canvas;
+        this.startX = Math.random() * canvas.width;
+        this.startY = Math.random() * canvas.height;
+        this.endX = Math.random() * canvas.width;
+        this.endY = Math.random() * canvas.height;
+        this.lineWidth = Math.floor(Math.random() * 15 + 1)
+    }
+    draw(context) {
+        context.lineWidth = this.lineWidth
+        context.beginPath();
+        context.moveTo(this.startX, this.startY);
+        context.lineTo(this.endX, this.endY);
+        context.stroke()
+    }
+}
+
+const line1 = new Line(canvas);
+line1.draw(ctx)
